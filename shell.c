0384a57c86f64544e19c9956b6de8a991b9e534b
viniciusgpereira <vini.gpereira2014@gmail.com>
2019-09-16 12:58:28 -0300

    Creates rode function and changes some code in readCommand function

--- shell.c
+++ shell.c
@@ -31,7 +31,6 @@ void liberaGeral(char* path) {
 
 void rodeVeja(char** path) {
     pid_t pid = fork();
-    int status;
     char** argv = path;
     char *envp[] = {"HOME=/", "PATH=/bin:/usr/bin", NULL};
     char bin[] = "/bin/";
@@ -40,7 +39,6 @@ void rodeVeja(char** path) {
 
     sprintf(program, "%s%s", bin, path[0]);
     argv[0] = program;
-
     if (pid == 0) {
         e = execve(argv[0], argv, envp);
     } else {
@@ -51,7 +49,24 @@ void rodeVeja(char** path) {
     free(program);
 }
 
-void readCommand(void) {
+void rode(char** path) {
+    pid_t pid = fork();
+    char** argv = path;
+    char *envp[] = {"HOME=/", "PATH=/bin:/usr/bin", NULL};
+    char bin[] = "/bin/";
+    char *program = malloc(strlen(bin) + strlen(path[0]) + 2);
+    
+    sprintf(program, "%s%s", bin, path[0]);
+    argv[0] = program;
+    if (pid == 0) {
+        setpgid(pid, 0);
+        execve(argv[0], argv, envp);
+    }
+
+    free(program);
+}
+
+int readCommand(void) {
     char *prog;
     char *param;
     char c;
@@ -103,14 +118,28 @@ void readCommand(void) {
         rodeVeja(command);
     }
 
+    /* rode */
+    if (!strcmp(prog, "rode")) {
+        rode(command);
+    }
+
+    /* sair do terminal */
+    if (!strcmp(prog, "exit")) {
+        return TRUE;
+    }
+
     free(prog);
     free(param);
     free(command);
+
+    return FALSE;
 }
 
-int main(int argc, char **argv  ) {
-    while(TRUE) {
-        readCommand();
+int main(int argc, char **argv) {
+    int flag = 0;
+
+    while(!flag) {
+        flag = readCommand();
     }
 
     return 0;
