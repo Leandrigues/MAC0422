142ba71ed7806046cc2ccfa21ab5a6cd1fc719fd
Leandro Rodrigues <rodriguesleandro@usp.br>
2019-09-17 09:42:26 -0300

    changes

--- shell.c
+++ shell.c
@@ -6,59 +6,62 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <signal.h>
 
 #define TRUE 1
 #define FALSE 0
-#define MAXTAM 100
+#define MAXTAM 20
 
-void protegePraCaramba(char* path) {
+void protegePraCaramba(char *path) {
     pid_t pid = fork();
 
     if(pid == 0) {
-        printf("Liberando\n");
         chmod(path, 00000);
         exit(0);
     }
 }
 
-void liberaGeral(char* path) {
+void liberaGeral(char *path) {
     pid_t pid = fork();
 
     if(pid == 0) {
-        printf("Liberando\n");
         chmod(path, 00777);
         exit(0);
     }
 }
 
-void rodeVeja(char** path) {
+void rodeVeja(char **path) {
     pid_t pid = fork();
-    char** argv = path;
+    char **argv = path;
     char *envp[] = {"HOME=/", "PATH=/bin:/usr/bin", NULL};
-    char bin[] = "/bin/";
-    char *program = malloc(strlen(bin) + strlen(path[0]) + 2);
     int e = 0;
-
-    sprintf(program, "%s%s", bin, path[0]);
-    argv[0] = program;
+    int status = 0;
 
     if (pid == 0) {
         e = execve(argv[0], argv, envp);
+        printf("shell: command not found: %s\n", path[0]);
     } else {
-        waitpid(pid, NULL, 0);
-        printf("=> programa '%s' retornou com código %d\n", program, e);
+        /* aguarda finalização do processo filho */ 
+        waitpid(pid, &status, 0);
+        printf("=> programa '%s' retornou com código %d\n", path[0], status);
     }
-
-    free(program);
 }
 
-void rode(char** path) {
+void rode(char **path) {
     pid_t pid = fork();
-    char** argv = path;
+    char **argv = path;
     char *envp[] = {"HOME=/", "PATH=/bin:/usr/bin", NULL};
-
+    int e;
+    
     if (pid == 0) {
-        execve(argv[0], argv, envp);
+        /* evita que CTRL+C interrompa o processo filho (o shell monopoliza o teclado) */
+        signal(SIGINT, SIG_IGN);
+        e = execve(argv[0], argv, envp);
+        free(argv);
+        printf("shell: command not found: %s\n", path[0]);
+    } else {
+        /* evita que o processo filho se torne um zombie */
+        signal(SIGCHLD, SIG_IGN);
     }
 }
 
@@ -67,11 +70,13 @@ int readCommand(void) {
     char *param;
     char c;
     int i = 0;
-
+    char **command;
+    char *parsed;
+    int index;
+    
     prog = malloc(MAXTAM);
+    printf("$ ");
     c = fgetc(stdin);
-
-    // 
     while(c != ' ' && c != '\n') {
         prog[i++] = c;
         c = fgetc(stdin);
@@ -97,9 +102,8 @@ int readCommand(void) {
     }
     param[i] = '\0';
 
-    char **command = malloc(sizeof(char *) * 10);
-    char *parsed;
-    int index = 0;
+    command = malloc(sizeof(char *) * 10);
+    index = 0;
     parsed = strtok(param, " ");
 
     /* transforma os parâmetros numa lista */
